199c4ff3840cd5a81b0ba947ab6f8b780d0e65b2
mobile-engineer <fajar.agungpramana@mareco.id>
2022-09-23 16:09:34 +0700

    Request permission for read gallery and camera access.

--- app/src/main/java/com/github/fajaragungpramana/ceritakita/ui/dialog/AppDialogImage.kt
+++ app/src/main/java/com/github/fajaragungpramana/ceritakita/ui/dialog/AppDialogImage.kt
@@ -1,7 +1,9 @@
 package com.github.fajaragungpramana.ceritakita.ui.dialog
 
+import android.Manifest
 import android.os.Bundle
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import com.github.fajaragungpramana.ceritakita.databinding.DialogImageBinding
 import com.github.fajaragungpramana.ceritakita.widget.app.AppBottomSheet
 
@@ -11,7 +13,30 @@ class AppDialogImage : AppBottomSheet<DialogImageBinding>() {
         DialogImageBinding.inflate(layoutInflater, viewGroup, false)
 
     override fun onCreated(savedInstanceState: Bundle?) {
+        val cameraPermission =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+                if (it) {
 
+                } else {
+
+                }
+            }
+
+        val galleryPermission =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+                if (it) {
+
+                } else {
+
+                }
+            }
+
+        viewBinding.mtvCamera.setOnClickListener {
+            cameraPermission.launch(Manifest.permission.CAMERA)
+        }
+        viewBinding.mtvGallery.setOnClickListener {
+            galleryPermission.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
     }
 
 }

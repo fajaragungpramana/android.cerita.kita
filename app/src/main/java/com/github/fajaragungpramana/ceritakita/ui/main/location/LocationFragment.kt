eb331665d817a402758081b7dfb4435bf1497548
mobile-engineer <fajar.agungpramana@mareco.id>
2022-10-14 14:58:17 +0700

    Setup static map view.

--- app/src/main/java/com/github/fajaragungpramana/ceritakita/ui/main/location/LocationFragment.kt
+++ app/src/main/java/com/github/fajaragungpramana/ceritakita/ui/main/location/LocationFragment.kt
@@ -2,15 +2,24 @@ package com.github.fajaragungpramana.ceritakita.ui.main.location
 
 import android.os.Bundle
 import android.view.ViewGroup
+import com.github.fajaragungpramana.ceritakita.R
 import com.github.fajaragungpramana.ceritakita.common.app.AppFragment
 import com.github.fajaragungpramana.ceritakita.databinding.FragmentLocationBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class LocationFragment : AppFragment<FragmentLocationBinding>() {
+class LocationFragment : AppFragment<FragmentLocationBinding>(), OnMapReadyCallback {
 
     override fun onViewBinding(container: ViewGroup?) =
         FragmentLocationBinding.inflate(layoutInflater, container, false)
 
     override fun onCreated(savedInstanceState: Bundle?) {
+        val mapFragment = childFragmentManager.findFragmentById(R.id.fcv_map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
 
     }
 
